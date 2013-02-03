53bea55a3b79a255a0eb4281078604757c3b5263
Bertho Stultiens <bertho@vagrearg.org>
2013-02-03 18:52:44 +0100

    Add license GPLv3

--- qflipdot/qflipdot.h
+++ qflipdot/qflipdot.h
@@ -1,5 +1,21 @@
-#ifndef __FLIPDOT_QFLIPDOT_H
-#define __FLIPDOT_QFLIPDOT_H
+/*
+ * This file is part of qFlipDot.
+ *
+ * qFlipDot is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * qFlipDot is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * qFlipDot. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __QFLIPDOT_QFLIPDOT_H
+#define __QFLIPDOT_QFLIPDOT_H
 
 #include <stdint.h>
 
